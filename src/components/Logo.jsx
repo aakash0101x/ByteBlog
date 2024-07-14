5789cc47d32246cefc7f38abd21d019b586f1fbc
Aakash <162045278+aakash0101x@users.noreply.github.com>
2024-07-15 00:16:58 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from ".../logo.png"
+
 function Logo({width = '50px',...prop}) {
   return (
-    <img src={logo} {...prop} width={50} className='rounded-xl' alt="" />
+    <img src="./logo.png" {...prop} width={50} className='rounded-xl' alt="" />
   )
 }
 
