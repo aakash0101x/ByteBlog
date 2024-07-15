47421c10af6b589fa33194c1ed528ad91fdcdf87
Aakash <162045278+aakash0101x@users.noreply.github.com>
2024-07-16 00:31:15 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-20 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
+                                Login to read blogs
                             </h1>
                         </div>
                     </div>
