dcfe21a0698809532ade2d0330428e2395fec81f
Aakash <162045278+aakash0101x@users.noreply.github.com>
2024-07-15 22:56:59 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,7 +80,7 @@ function Home() {
         
         <div className='w-full py-12 linear_grad'>
             
-            <div className='text-center'>
+            <div className='text-center py-2'>
               <h1 className='text-3xl font-extralight font-bold text-white'>My Feed</h1>
             </div>
 
